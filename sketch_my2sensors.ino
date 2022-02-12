bf873356cc6278f313b36ca1bd3af45946c54e39
Kuldyaev Viacheslav <slavalion1@gmail.com>
2022-02-13 01:39:11 +0300

    Update sketch_my2sensors.ino

--- sketch_my2sensors.ino
+++ sketch_my2sensors.ino
@@ -4,14 +4,18 @@
 Adafruit_BMP085 bmp;
 AM2320_asukiaaa mySensor;
 
+unsigned long last_time;
+
 void setup() {
   pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
   Serial.begin(9600);
-  while(!Serial);
+//  while(!Serial);
   Serial.println("started");
 }
 
 void loop() {
+  String meas = "#MEAS";
   digitalWrite(12, HIGH); 
   delay(1000);
   Wire.begin();
@@ -19,47 +23,31 @@ void loop() {
   if (mySensor.update() != 0) {
     Serial.println("Error: Cannot update sensor values.");
   } else {
-    Serial.println("temperatureC: " + String(mySensor.temperatureC) + " C");
-    Serial.println("temperatureF: " + String(mySensor.temperatureF) + " F");
-    Serial.println("humidity: " + String(mySensor.humidity) + " %");
+    meas+=":t=" + String(mySensor.temperatureC) + "C" +";h=" + String(mySensor.humidity) + "%";
+    int val = analogRead(A0);
+    meas+=";l="+String(val);
   }
-  Serial.println("at " + String(millis()) + " ms");
-  Serial.println("");
   Wire.end();
-  delay(5000);
+  delay(2000);
   Wire.begin();
   if (!bmp.begin()) {
   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   while (1) {}
   }
- Serial.print("Temperature = ");
-  Serial.print(bmp.readTemperature());
-  Serial.println(" *C");
-  
-  Serial.print("Pressure = ");
-  Serial.print(bmp.readPressure());
-  Serial.println(" Pa");
-  
-  // Calculate altitude assuming 'standard' barometric
-  // pressure of 1013.25 millibar = 101325 Pascal
-  Serial.print("Altitude = ");
-  Serial.print(bmp.readAltitude());
-  Serial.println(" meters");
-
-  Serial.print("Pressure at sealevel (calculated) = ");
-  Serial.print(bmp.readSealevelPressure());
-  Serial.println(" Pa");
-
-// you can get a more precise measurement of altitude
-// if you know the current sea level pressure which will
-// vary with weather and such. If it is 1015 millibars
-// that is equal to 101500 Pascals.
-  Serial.print("Real altitude = ");
-  Serial.print(bmp.readAltitude(101500));
-  Serial.println(" meters");
-  
-  Serial.println();
+  meas+=";T="+String(bmp.readTemperature())+" *C;p="+String(bmp.readPressure()/133)+"mm$#";
+  Serial.println(meas);
   Wire.end();
   digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-  delay(15000);
+  Wire.begin();
+  delay(1000);
+  digitalWrite(11, HIGH); 
+  last_time=millis();
+  while(millis() < last_time+15000){
+    if (Serial.available()){
+        Serial.println(String(Serial.readString()));
+       } 
+  }
+  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
+  Wire.end();
+  delay(500);
 }
