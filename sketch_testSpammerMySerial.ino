86bb492095385f8bd4bcb9565ffacfd129d923ee
Kuldyaev Viacheslav <slavalion1@gmail.com>
2022-02-15 00:07:38 +0300

    Add files via upload

---
+++ sketch_testSpammerMySerial.ino
@@ -0,0 +1,25 @@
+
+#include "Wire.h"                // библиотека соединений модулей
+#define RX 11                     // Определяем пин UART RX
+#define TX 12                     // Определяем пин UART TX
+#include <SoftwareSerial.h>      // Подключаем библиотеку программного UART
+SoftwareSerial mySerial(RX, TX); // Создаём объект программного UART
+void setup() {
+  Serial.begin(9600);                               // стартуем связь с портом
+  while (!Serial) {delay(10);}                      // ждем запуск порта Serial
+  
+ //-------Запуск Serial ----------------------------//
+  mySerial.begin(9600);                             // стартуем связь с портом
+  while (!mySerial) {delay(10);}                      // ждем запуск порта Serial
+                                   // устанавливаем соединения 
+ //--------Сканируем подключенные устройства--------// 
+  Wire.begin();
+}
+
+void loop() {
+  if (mySerial.available()){
+    Serial.print(mySerial.readString());
+   } 
+   mySerial.println("Test");
+   delay(2000);
+}
